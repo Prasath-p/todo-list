f12d4efebd996fbf68e762041f2922f6a5d998c3
Prasath P <prasath2019cs@gmail.com>
2022-02-16 16:26:44 +0530

    created final app

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# todo-list
+Making todo-list using react
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TodoList from './Component/TodoList';
+
+ function App() {
+  return (
+    <div className="container">
+      <TodoList />
+    </div>
+  );
+}
+export default App;
+
